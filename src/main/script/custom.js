0ea1a2c8da64c2cb5da04fa72a9b0de74bb7178e
Jakub Wojcik <jwojcik@atex.com>
2019-02-14 11:14:06 +0000

    PRS-326-Added ability to copy to clipboard from within the result set.

--- src/main/script/custom.js
+++ src/main/script/custom.js
@@ -1,14 +1,28 @@
 jQuery(document).ready(function() {
 
-    jQuery(window).click(function(e){
+    updateSearchFrameEventHandlers();
 
-        if($(e.target).parent("button.clipboard")){
-            setTimeout(clipboardAdapterProcessor, 500)
+    jQuery(window).on('click', 'button.clipboard > img', function(){
+        setTimeout(clipboardAdapterProcessor, 500)
+    });
+
+    jQuery('div.searchbox input.searchInput').keypress(function (e) {
+        if (e.which == 13) {
+            setTimeout(updateSearchFrameEventHandlers,200);
         }
+    });
 
+    jQuery(document).on('click', '.toolbar-button > a.gridViewButton, .toolbar-button > a.listViewButton', function(){
+        setTimeout(updateSearchFrameEventHandlers,200);
     });
 
 });
+
+function updateSearchFrameEventHandlers(){
+    jQuery('div.copy_button > a > img').add(jQuery('td.copy > div > a > img')).on('click',function() {
+        setTimeout(clipboardAdapterProcessor, 500)
+    });
+}
 
 function clipboardAdapterProcessor() {
     var xhr = new XMLHttpRequest();
@@ -16,4 +30,5 @@ function clipboardAdapterProcessor() {
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send();
 }
+
 
